5815f39f06e556100b59aa9fabb77155c0151135
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2020-10-20 01:20:59 +0900

    feat: layout home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,38 +1,47 @@
 import React, { FC, useState } from 'react'
-import { Text, Box, useInput } from 'ink'
+import { Text, Box, Spacer, useInput, useApp } from 'ink'
 import { UncontrolledTextInput } from 'ink-text-input'
 import { TaskInput } from './TaskInput'
 
 export const Home: FC = () => {
-  const [taskList, setTaskList] = useState<string[]>([])
-  const [enterMode, setEnterMode] = useState(false)
+  const { exit } = useApp()
 
-  useInput((input, key) => {
+  const currentCollectionName = 'General'
+  const todoCount = 0
+  const doneCount = 0
+  const taskList = ['a sample task']
+
+  useInput((_, key) => {
     if (key.return) {
-      setEnterMode(true)
+      exit()
     }
   })
 
   return (
     <Box flexDirection="column" minHeight={10}>
-      <Box justifyContent="center">
-        <Text>What To Do</Text>
+      {/* Header */}
+      <Box flexDirection="column">
+        <Text bold={true}>Collection: {currentCollectionName} (1/1)</Text>
+        <Box>
+          <Text>Todo: {todoCount}</Text>
+          <Box marginRight={2} />
+          <Text>Done: {doneCount}</Text>
+        </Box>
       </Box>
-      {enterMode && (
-        <TaskInput
-          onSubmit={(value) => {
-            setTaskList([...taskList, value])
-            setEnterMode(false)
-          }}
-        />
-      )}
-      <Box paddingX={1} flexDirection="column" borderStyle="round">
+
+      {/* Body */}
+      <Box paddingX={1} marginBottom={1} flexDirection="column" borderStyle="doubleSingle">
         {taskList.map((task) => {
           return <Text key={task}>{`  ${task}`}</Text>
         })}
       </Box>
-      <Box>
-        <Text>Enter: Save, Arrow keys: Move, Space: change task status</Text>
+
+      {/* Guide */}
+      <Box flexDirection="column">
+        <Text>[ArrowUp/ArrowDown]: move cursor</Text>
+        <Text>[ArrowLeft/ArrowRight]: change collection</Text>
+        <Text>[Space]: change task status</Text>
+        <Text>[Enter]: save and quit</Text>
       </Box>
     </Box>
   )
